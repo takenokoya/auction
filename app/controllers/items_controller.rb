ff681978193598af03baa086c576e93d618336c6
takenokoya <takuya1002@gmail.com>
2019-03-09 17:33:54 +0900

    create create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,9 +13,15 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to @item
   end
 
-
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
+  end
 
 end
 
